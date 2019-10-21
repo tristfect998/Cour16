24730fd70d62b3e7e6121548238368b3b05438e4
B533P-15\533 <tristan.fecteau@effecto.ca>
2019-10-21 17:05:17 -0400

    Partie 2 fini final

---
+++ Cours16/Assets/PostProcessingShader/CameraEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour {
+    Material material;
+	
+	void Awake () {
+        material = new Material(Shader.Find("Hidden/Shader"));
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
